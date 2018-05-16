69041bfb8656d1744fc3c57a3292055ab177923d
Akshathaholla91 <akshathaholla91@gmail.com>
2018-05-16 12:24:20 +0530

    Changes

--- main.py
+++ main.py
@@ -150,8 +150,7 @@ def run():
         layers_output=layers(layer3_out,layer4_out, layer7_out, num_classes)
         logits, train_op, cross_entropy_loss = optimize(layers_output, correct_label, learning_rate, num_classes)
         # TODO: Train NN using the train_nn function
-         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label,
-                 keep_prob, learning_rate)
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label,keep_prob, learning_rate)
         # TODO: Save inference data using helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
